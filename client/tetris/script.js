5b232bf6350a220c9f0eacb428f7ff052fd27325
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-09-30 19:47:52 -0400

    defining main js variables

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMCcontentLoaded', () => {
-    
+  const grid=document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
+  const width = 10;
 })
