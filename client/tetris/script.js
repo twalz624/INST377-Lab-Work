a81b0ee1f5634a90e1f0aeb34bef7ab22fc06bf7
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-09-30 19:59:01 -0400

    created tetromino arrays and styling

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -4,4 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
+
+  // Tetrominoes //
+  const lTetromino = [
+    [1, width+1, width*2+1,2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ];
+  const zTetromino = [
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
+  ];
+  const oTetromino = [
+    [0,1,width, width+1],
+    [0,1,width, width+1],
+    [0,1,width, width+1],
+    [0,1,width, width+1]
+  ];
+  const iTetromino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ];
+
+  const theTetrominoes = [lTetromino, zTetromino, oTetromino, iTetromino];
 });
