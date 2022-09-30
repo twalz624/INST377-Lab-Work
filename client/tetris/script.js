58ef8bdb8c1afd7a40d905094cb70f10dcfdb48f
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-09-30 19:48:51 -0400

    mynor syntax corrections

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMCcontentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const grid=document.querySelector('.grid');
   let squares = Array.from(document.querySelectorAll('.grid div'));
   const ScoreDisplay = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
-})
+});
