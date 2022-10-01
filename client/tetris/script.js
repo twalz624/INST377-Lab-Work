e39b450c8bef278acf4000617e639fee3f48d07b
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-09-30 20:29:24 -0400

    create function to move shape left

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -73,5 +73,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   // make the tetromino move down every second
   timerId = setInterval(moveDown, 1000);
-  
+
+  // move the tetromino left, unless it hits a wall
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(index = (currentPosition + index) % width === 0);
+
+    if (!isAtLeftEdge) currentPosition -= 1;
+
+    if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition += 1;
+    }
+    draw();
+  }
 });
