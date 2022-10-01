5304d97f36386b23e3c88ee9adb04d9ad58c05d6
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-09-30 20:24:53 -0400

    added function to stop and replace tetromino when it hits the bottom

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -52,13 +52,26 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  // make the tetromino move down every second
-  timerId = setInterval(moveDown, 1000);
+  // freeze function //
+  function freeze() {
+    if (current.some((index) => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach((index) => squares[currentPosition + index].classList.add('taken'));
+      // start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
 
   // move down function
   function moveDown() {
     undraw();
     currentPosition += width;
     draw();
+    freeze();
   }
+  // make the tetromino move down every second
+  timerId = setInterval(moveDown, 1000);
+  
 });
