ea20673f021968d21c5a794eed780128ff9d8046
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-10-07 18:34:17 -0400

    incorporated script and changed so searching name works

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -35,13 +35,14 @@ async function mainEvent() { // the async keyword means we can make API requests
       // It does not include any of your form values, though
     */
 
-    const results = await fetch('/api/foodServicePG');
+    // const results = await fetch('/api/foodServicePG');
+    const fetchQuery = new URLSearchParams(formProps);
+    const results = await fetch(`/api/foodServicePG?${fetchQuery}`);
     /*
    ## Get request with query parameters
 
-      const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
 
-      The above request uses "string interpolation" to include an encoded version of your form values
+      Above request uses "string interpolation" to include an encoded version of your form values
       It works because it has a ? in the string
       Replace line 37 with it, and try it with a / instead to see what your server console says
 
