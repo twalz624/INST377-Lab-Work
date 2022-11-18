80ff73a786a27369dcb75652cae5ab9f29924d84
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-11-18 13:45:50 -0500

    set map coords to college park

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -76,7 +76,7 @@ function filterList(array, filterInputValue) {
 
 function initMap() {
   console.log('initMap');
-  const map = L.map('map').setView([51.505, -0.09], 13);
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
